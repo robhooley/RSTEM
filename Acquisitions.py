f2310e7614f5fbd0a99e33b958e650ee1a5d5dd5
robert.hooley <robert.hooley@tescan.com>
2024-01-08 18:13:00 +0100

    updates

--- Acquisitions.py
+++ Acquisitions.py
@@ -1,5 +1,5 @@
 
-from .expert_pi import grpc_client
+from expert_pi import grpc_client
 from expert_pi.controllers import scan_helper
 from expert_pi.stream_clients import cache_client
 from expert_pi.grpc_client.modules._common import DetectorType as DT
@@ -19,6 +19,7 @@ def drift_corrected_imaging(num_frames, output="Summed",shift_method="ML",num_pi
     fov = fov*1e-6
     print("Field of View in um",fov)
 
+    #TODO add in option to calulcate series dose?
 
     if num_pixels == None:
         num_pixels = 1024
@@ -39,6 +40,7 @@ def drift_corrected_imaging(num_frames, output="Summed",shift_method="ML",num_pi
     ADF_images_list.append(initial_ADF_image)
     image_offsets.append(initial_shift)
 
+    #TODO check other shift measurement methods
     shift_method = shift_measurements.Method.PatchesPass2
 
     for frame in range(len(num_frames)):
@@ -46,28 +48,30 @@ def drift_corrected_imaging(num_frames, output="Summed",shift_method="ML",num_pi
         scan_id = scan_helper.start_rectangle_scan(pixel_time=pixel_time, total_size=num_pixels, frames=1,
                                                    detectors=[DT.BF, DT.HAADF, DT.EDX1, DT.EDX0])
 
-        header, data = cache_client.get_item(scan_id, num_pixels ** 2)
-        s = grpc_client.illumination.get_shift(grpc_client.illumination.DeflectorType.Scan)
+        header, data = cache_client.get_item(scan_id, num_pixels ** 2) #retrive image from cache
+        BF_image = data["stemData"]["BF"].reshape(num_pixels, num_pixels) #reshape BF image
+        BF_images_list.append(BF_image) #add to list
+        ADF_image = data["stemData"]["HAADF"].reshape(num_pixels, num_pixels) #reshape ADF image
+        ADF_images_list.append(ADF_image) #add to list
+
+        s = grpc_client.illumination.get_shift(grpc_client.illumination.DeflectorType.Scan) #get current beam shifts
 
         # apply drift correction between images:
-        img2 = data["stemData"]["BF"].reshape(1024, 1024)
-        shift = shift_measurements.get_offset_of_pictures(initial_BF_image, img2, fov, method=shift_method)
-        shift = np.dot(R, shift)  # rotate back
+        shift = shift_measurements.get_offset_of_pictures(initial_BF_image, BF_image, fov, method=shift_method) #measure offset of images
+        shift = np.dot(R, shift)  # rotate shifts back to scanning axes
         #print(i, (s0['x'] - s['x']) * 1e9, (s0['y'] - s['y']) * 1e9)
-        grpc_client.illumination.set_shift({"x": s['x'] - shift[0] * 1e-6, "y": s['y'] - shift[1] * 1e-6},
-                                           grpc_client.illumination.DeflectorType.Scan)
+        grpc_client.illumination.set_shift(
+                {"x": s['x'] - shift[0] * 1e-6, "y": s['y'] - shift[1] * 1e-6},grpc_client.illumination.DeflectorType.Scan) #apply shifts in microns to existing shits
 
-        BF_image = data["stemData"]["BF"].reshape(num_pixels, num_pixels)
-        BF_images_list.append(BF_image)
-        ADF_image = data["stemData"]["HAADF"].reshape(num_pixels, num_pixels)
-        ADF_images_list.append(ADF_image)
+
+
 
 
     BF_summed = sum(BF_images_list)
     ADF_summed = sum(ADF_images_list)
 
-    if output == "Summed" or None:
+    if output == "Summed" or None or False:
         return BF_summed, ADF_summed
 
-    if output == "All":
+    if output == "All" or True:
         return BF_images_list, ADF_images_list
